0b0a05f50b4759b4e0317620d72cea23292c4f31
zhero <zheronkina86@gmail.com>
2024-08-05 00:02:06 +0300

    All project 1

--- spring/src/main/java/pro/sky/spring/service/Basket.java
+++ spring/src/main/java/pro/sky/spring/service/Basket.java
@@ -3,7 +3,10 @@ package pro.sky.spring.service;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Component
 @SessionScope
